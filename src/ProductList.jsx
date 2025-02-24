4affe77ba5b9c60db588908dd97836e32b6b8b30
mlj27611 <mlj27611@gmail.com>
2025-02-24 12:35:59 -0500

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,11 +1,15 @@
 import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { createSlice } from '@reduxjs/toolkit';
+import { addItem } from './CartSlice';
+import { useDispatch,useSelector } from 'react-redux';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({});
+    const [showPlants, setShowPlants] = useState(false);
+    const dispatch = useDispatch();
+    const CartItems = useSelector((state) => state.cart.items);
+    const [addedToCart, setAddedToCart] = useState({}); // State to control the visibility of the About Us page
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -48,8 +52,8 @@ function ProductList() {
                 }
             ]
         },
-        
-         {   category: "Aromatic Fragrant Plants",
+        {
+            category: "Aromatic Fragrant Plants",
             plants: [
                 {
                     name: "Lavender",
@@ -242,21 +246,32 @@ const handlePlantsClick = (e) => {
     setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
     setShowCart(false); // Hide the cart when navigating to About Us
 };
-const handleAddToCart = (product) => {
+
+   const handleContinueShopping = (e) => {
+    e.preventDefault();
+    setShowCart(false);
+  };
+   
+  const calculateTotalQuantity = () => {
+    return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+  };
+  const [totalQuantity, setTotalQuantity] = useState(calculateTotalQuantity());
+  useEffect(() => {
+    setTotalQuantity(
+        calculateTotalQuantity()
+    );
+  }, [CartItems])
+
+  
+
+  const handleAddToCart = (product, e) => {
     dispatch(addItem(product));
     setAddedToCart((prevState) => ({
        ...prevState,
        [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
      }));
   };
-   const handleContinueShopping = (e) => {
-    e.preventDefault();
-    setShowCart(false);
-  };
-  
 
-
-   
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -274,14 +289,12 @@ const handleAddToCart = (product) => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg><div className='cart_quantity_count'>{totalQuantity}</div></h1></a></div>
             </div>
         </div>
         {!showCart? (
-          
         <div className="product-grid">
-      
-              {plantsArray.map((category, index) => (
+                {plantsArray.map((category, index) => (
     <div key={index}>
         <h1><div>{category.category}</div></h1>
         <div className="product-list">
@@ -289,14 +302,15 @@ const handleAddToCart = (product) => {
             <div className="product-card" key={plantIndex}>
                 <img className="product-image" src={plant.image} alt={plant.name} />
                 <div className="product-title">{plant.name}</div>
+                <div className='product-description'>{plant.description}</div>
+                <div className='product-cost'>{plant.cost}</div>
                 {/*Similarly like the above plant.name show other details like description and cost*/}
-                <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                <button  className={`product-button $[addedToCart[plant.name] ? 'added-to-cart':''}`} onClick={(e) => handleAddToCart(plant)} disabled={addedToCart[plant.name]} >{addedToCart[plant.name] ? "Added to cart" : "Add to cart"}</button>
             </div>
             ))}
         </div>
     </div>
-    ))}  
-
+    ))}
 
         </div>
  ) :  (
@@ -306,4 +320,4 @@ const handleAddToCart = (product) => {
     );
 }
 
-export default ProductList;
+export default Productlist;
