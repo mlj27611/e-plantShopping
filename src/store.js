51f544d0a7baa90678720d9058736cb0de1507d2
mlj27611 <mlj27611@gmail.com>
2025-02-24 12:42:44 -0500

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
+
+// Configure the Redux store
+const store = configureStore({
     reducer: {
         cart: cartReducer,
     },
 });
-const { name, quantity } = action.payload;
-const itemToUpdate = state.items.find(item => item.name === name);
-if (itemToUpdate) {
-  itemToUpdate.quantity = quantity;
-}
-export default store
+
+export default store;
